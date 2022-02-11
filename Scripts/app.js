48f0a0d1cb7a066d2c690b926ddce9dcd2ad8775
pypatel593m <pypatel593m@gmail.com>
2022-02-11 15:28:15 -0500

    added ICE-5

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,231 +1,120 @@
-// IIFE -- Immediately Invoke Function Expression
+// IIFE -- Immediately Invoked Function Expression
 // AKA Anonymous Self-Executing Function
 (function()
 {
-    
-    // Adding the human resource link to navbar
-    function LinkHumanResource()
+
+    function DisplayAboutPage()
     {
-
-        let NewLink = `<a class="nav-link" href="about.html"> <i class="fas fa-male"></i> Human Resource</a>`;
-        let DocumentBody = document.getElementsByTagName("li")[4];
-        let abotUsPara = document.createElement("abotUsPara");
-        abotUsPara.setAttribute("class", "center");
-        abotUsPara.innerHTML = NewLink;
-        DocumentBody.appendChild(abotUsPara);
-
+        console.log("About Us Page");
     }
 
-
-    function ChangeProductsToProjects()
-    {
-        let NewLink = `<a class="nav-link" href="products.html"> <i class="fas fa-project-diagram"></i>Projects</a>`;
-        let DocumentBody = document.getElementsByTagName("li")[1];
-        let abotUsPara = document.createElement("abotUsPara");
-        abotUsPara.setAttribute("class", "center");
-        abotUsPara.innerHTML = NewLink;
-        DocumentBody.appendChild(abotUsPara);
-        document.getElementsByTagName("a")[2].remove();
-    }
-
-    function Background() 
-    {
-        document.body.style.backgroundImage = "url('.././image/HomeBackground.jpg')";
-        document.body.style.color = "white";
-    }
-     function navbar(){
-     
-        let DocumentBody = document.body;
-       
-        let detailParagraph = document.createElement("detailParagraph");
-        let abotUsPara = `<nav class="navbar fixed-bottom navbar-light bg-dark">
-        <div align="right">
-        ©️ CopyRight 2022 
-        </div>
-     </nav>`;
-  
-        // step-3 configure
-        detailParagraph.setAttribute("class", "container");
-        // step-4 performation
-  
-        detailParagraph.innerHTML = abotUsPara;
-        DocumentBody.appendChild(detailParagraph);
-     }
-     
     function DisplayProductsPage()
     {
         console.log("Products Page");
-   
-        //step-1 - get a reference to entry point.
-     let DocumentBody = document.body;
-     let detailParagraph = document.createElement("detailParagraph");
-     let detailParagraphParagraph = `<table id="tableInfo" width="90%">
-     <tr>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(1)Tarneeb Card Game</h2></th>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(2)The Tic Tac Toe Game</h2></th>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(3)This Website</h2></th>
-     </tr>
-     <tr>
-        <td align="center"><h6 style="margin-left: 100px;">This is our on going project, In this project we are 
-        developing one card game called tarneeb, and we are using c# WPF project for it. </h3></td>
-        <td align="center"><h6 style="margin-left: 100px;">This was our first ever experience of game developing,
-        We built this small little game a year ago, and we used c# windows forms for it.</h3></td>
-        <td align="center"><h6 style="margin-left: 100px;">Yes, this website is also one of the best projects that we worked on
-        and as long as we are making it, it gets more and more intresting, we are trying our best to make it look good,
-        layout wise and functionally as well.</h3></td>
-     </tr>
-      </table>`;
-
-     // step-3 configure
-     detailParagraph.setAttribute("class", "container");
-     // step-4 performation
-     detailParagraph.innerHTML = detailParagraphParagraph;
-     DocumentBody.appendChild(detailParagraph);
-     
-
     }
+
     function DisplayServicesPage()
     {
         console.log("Services Page");
-        let DocumentBody = document.body;
-     let detailParagraph = document.createElement("detailParagraph");
-     let detailParagraphParagraph = `<table id="tableInfo" width="90%">
-     <tr>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(1)Software Developing</h2></th>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(2)Web Designing</h2></th>
-        <th width="33.3%"><h2 style="margin-left: 100px;">(3)Mobile Computing</h2></th>
-     </tr>
-     <tr>
-        <td align="center"><h6 style="margin-left: 100px;">We do implies all the necessary 
-        skills like mathematical aptitude, programming languages and very obvious system devlopment 
-        skills which means it covers all the major portion starting from proposal to launching software 
-        and even maintaining it</h3></td>
-        <td align="center"><h6 style="margin-left: 100px;">It is one of the vital element in field 
-        of IT. We rate this as best skill we possess among all we know. We are now comfortable working 
-        with html, php, javascript, CSS and database too.</h3></td>
-        <td align="center"><h6 style="margin-left: 100px;">It is like cloud computing. 
-        We are best to establish the relation between servers, pcs, network and good at things like maintaining IP 
-        address of network or PC and can solve any problem related mobile 
-        computing (network issue or server issues).</h3></td>
-     </tr>
-      </table>`;
-
-     // step-3 configure
-     detailParagraph.setAttribute("class", "container");
-     // step-4 performation
-     detailParagraph.innerHTML = detailParagraphParagraph;
-     DocumentBody.appendChild(detailParagraph);
-      
-     
-
     }
-    function DisplayAboutUsPage()
-    {
-        console.log("About Us Page");
-        let DocumentBody = document.body;
-        let abotUsPara = document.createElement("abotUsPara");
-        let WelcomePara = `<table id="tableInfo" width="90%">
-        <tr>
-           <th width="33.3%"><h3 style="margin-left: 100px;">Our goal for this lab is to shows our best work and our skills of web designing and editing.
-           We Both contributed with our efforts providing majority of work as per requirements. This lab is result
-           of best teamwork as well as proper division of task among the team. 
-      
-   </h3></th>
-        </tr>
-         </table>`;
-        // step-3 configure
-        abotUsPara.setAttribute("class", "center");
-        abotUsPara.innerHTML = WelcomePara;
-        DocumentBody.appendChild(abotUsPara);
-    }
+
 
     function DisplayHomePage()
     {
-        console.log("Home Page.")
-        
-        let AboutUsButton = document.getElementById("AboutUsButton");
+        console.log("Home Page");
 
-        AboutUsButton.addEventListener("click", function()
+        $("#AboutUsButton").on("click", function()
         {
-            //redirect to about page
             location.href = "about.html";
         });
 
-        let DocumentBody = document.body;
-     let abotUsPara = document.createElement("abotUsPara");
-     let WelcomePara = `<table id="tableInfo" width="90%">
-     <tr>&nbsp
-     </tr>
-     <tr>
-        <th width="33.3%"><h3 style="margin-left: 100px;">You have reached the most amazing website, 
-        look around and you will find many interesting things. This is home page of the website, if you want to know more about us, 
-        click on that blue button. 
-</h3></th>
-     </tr>
-     <tr>
-     <th>
-     <img style="margin-left: 100px;" style="border-radius: 40px 40px 0 0;" src= "./image/welcome.jpg" width="550" height="350"style="float: center;">
-     </th>
-     </tr>
-      </table>`;
-     // step-3 configure
-     abotUsPara.setAttribute("class", "center");
-     abotUsPara.innerHTML = WelcomePara;
-     DocumentBody.appendChild(abotUsPara);
+        $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
+        //Article.innerHTML = ArticleParagraph;
+        $("body").append(`<article class="container">
+        <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>
+        </article>`);
     }
 
-    function Redirect() {
-        let count = 3;
-        setInterval(function(){
-            count--;
-            if (count == 0)
-            {
-                window.location = "contact-list.html";
-            }
-        }, 1000);
-        
-      }
+    /**
+     * Adds a Contact Object to localStorage
+     *
+     * @param {string} fullName
+     * @param {string} contactNumber
+     * @param {string} emailAddress
+     */
+    function AddContact(fullName, contactNumber, emailAddress)
+    {
+        let contact = new core.Contact(fullName, contactNumber, emailAddress);
+        if(contact.serialize())
+        {
+            let key = contact.FullName.substring(0, 1) + Date.now();
 
-    
-    function DisplayContactUsPage()
+            localStorage.setItem(key, contact.serialize());
+        }
+    }
+
+    /**
+     * This method validates an input text fields in the form
+     * and displays an error in message Area div element
+     * 
+     * @param {string} input_field_ID 
+     * @param {RegExp} regular_expression 
+     * @param {string} errpr_message 
+     */
+    function ValidateField(input_field_ID, regular_expression, error_message)
+    {
+        let messageArea = $("#messageArea").hide();
+
+        $("#" + input_field_ID).on("blur", function()
+        {
+            let inputTextContent = $(this).val(); 
+            if(!regular_expression.test(inputTextContent))
+            {
+                $(this).trigger("focus").trigger("select");
+                messageArea.addClass("alert alert-danger").text(error_message).show();
+            }
+            else 
+            {
+                messageArea.removeAttr("class").hide();
+            }
+        });
+    }
+
+    function ContactFormValidation()
+    {
+        ValidateField("fullName", /^([A-Z][a-z]{1,3}.?\s)?([A-Z][a-z]{1,25})((\s|,|-)([A-Z][a-z]{1,25}))*(\s|,|-)([A-Z][a-z]{1,25})$/, "Please enter a valid Full Name. This must include at least a Capitalized first name followed by a Capitalized last Name.");
+        ValidateField("contactNumber", /^(\+\d{1,3}[\s-.])?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]?\d{4}$/, "Please enter a valid Contact Number. Contact Number Example: (905) 555-5555");
+        ValidateField("emailAddress", /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/, "Please enter a valid Email address.");
+    }
+
+    function DisplayContactPage()
     {
         console.log("Contact Us Page");
+        
+        ContactFormValidation();
+        
 
         let sendButton = document.getElementById("sendButton");
-        let subscribeCheckBox = document.getElementById("subscribeCheckBox");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
 
-        //localStorage.setItem("1", "Parth");
-        //console.log(localStorage.getItem("1"));
-        //localStorage.removeItem("1");
-        //console.log(localStorage.length);
-
-        sendButton.addEventListener("click", function()
+        sendButton.addEventListener("click", function(event)
         {
-            
-            //event.preventDefault(); // for debugging
+            //event.preventDefault();
 
-            if(subscribeCheckBox.checked)
+            if(subscribeCheckbox.checked)
             {
-                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
-                if(contact.serialize())
-                {
-                    let key = contact.FullName.substring(0, 1) + Date.now();
-
-                    localStorage.setItem(key, contact.serialize());
-                    Redirect();
-                }
+                AddContact(fullName.value, contactNumber.value, emailAddress.value);
             }
-            
-        })
+        });
     }
 
     function DisplayContactListPage()
     {
         console.log("Contact-List Page");
-        if(localStorage.length > 0) // check if local storage has something in it
+
+        if(localStorage.length > 0) // check if localStorage has something in it 
         {
             let contactList = document.getElementById("contactList");
-            contactList.setAttribute("style", "color: White; border: 0px;")
 
             let data = "";
 
@@ -233,12 +122,12 @@
 
             let index = 1;
 
-            // for every key in keys collection
+            //for every key in the keys collection loop
             for(const key of keys)
             {
-                let contactData = localStorage.getItem(key); // retrieves contact data from local storage
+                let contactData = localStorage.getItem(key); // retrieve contact data from localStorage
 
-                let contact = new Contact(); // creat an empty contact object 
+                let contact = new core.Contact(); // create an empty Contact Object
                 contact.deserialize(contactData);
 
                 data += `<tr>
@@ -246,70 +135,159 @@
                 <td>${contact.FullName}</td>
                 <td>${contact.ContactNumber}</td>
                 <td>${contact.EmailAddress}</td>
-                <td></td>
-                <td></td>
-                </tr>`
+                <td class="text-center"><button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button></td>
+                <td class="text-center"><button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button></td>
+                </tr>
+                `;
 
-                index ++;
+                
+                
+                index++;
             }
 
             contactList.innerHTML = data;
+
+            $("#addButton").on("click", () =>
+            {
+                location.href = "edit.html#add";
+            });
+
+            $("button.delete").on("click", function()
+            {
+                if(confirm("Are you sure?"))
+                {
+                    localStorage.removeItem($(this).val());
+                }
+                
+                location.href = "contact-list.html";
+            });
+
+            $("button.edit").on("click", function() 
+            {
+                location.href = "edit.html#" + $(this).val();
+            });
         }
     }
 
-    function DisplayParthPage(){
-        console.log("Parth's page");
-        
-     }
-  
-      function DisplayYashPage(){
-         console.log("Yash's page");
-         
-      }
+    function DisplayEditPage()
+    {
+        console.log("Edit Page");
 
-  
+        ContactFormValidation();
+
+        let page = location.hash.substring(1);
+
+        switch(page)
+        {
+            case "add":
+                {
+                    $("main>h1").text("Add Contact");
+
+                    $("#editButton").html(`<i class="fas fa-plus-circle fa-lg"></i> Add`);
+
+                    $("#editButton").on("click", (event) => 
+                    {
+                        event.preventDefault();
+                        // Add Contactt
+                        AddContact(fullName.value, contactNumber.value, emailAddress.value);
+                        // Refresh the contact-list page
+                        location.href ="contact-list.html";
+                    });
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href ="contact-list.html";
+                    });
+
+                }
+                break;
+            default:
+                {
+                    // get the contact info from localStorage
+                    let contact = new core.Contact();
+                    contact.deserialize(localStorage.getItem(page));
+
+                    // display the contact info in the edit form
+                    $("#fullName").val(contact.FullName);
+                    $("#contactNumber").val(contact.ContactNumber);
+                    $("#emailAddress").val(contact.EmailAddress);
+
+                    // when Edit is pressed - update the contact
+                    $("#editButton").on("click", (event)=>
+                    {
+                        event.preventDefault();
+
+                        // get any changes from the form
+                        contact.FullName = $("#fullName").val();
+                        contact.ContactNumber = $("#contactNumber").val();
+                        contact.EmailAddress = $("#emailAddress").val();
+
+                        // replace the item in localStorage
+                        localStorage.setItem(page, contact.serialize());
+
+                        // return to the contact-list
+                        location.href ="contact-list.html";
+                    });
+
+                    $("#cancelButton").on("click", () =>
+                    {
+                        location.href ="contact-list.html";
+                    });
+                    
+                }
+                break;
+        }
+    }
+
+
+    function DisplayLoginPage()
+    {
+        console.log("Login Page.");
+    }
+
+    function DisplayRegisterPage()
+    {
+        console.log("Register Page.");
+    }
 
     // named function
     function Start()
     {
-        navbar();
-        Background();
-        LinkHumanResource();
-        ChangeProductsToProjects();
-        console.log("App Started!");
+        console.log("App Started!!");
 
-        switch(document.title)
+        switch (document.title) 
         {
-            case "Home":
-                DisplayHomePage();
+          case "Home":
+            DisplayHomePage();
             break;
-            case "Our Products":
-                DisplayProductsPage();
-                break;
-            case "Our Services":
-                DisplayServicesPage();
-                break;
-            case "About Us":
-                DisplayAboutUsPage();
-                break;
-            case "Contact Us":
-                DisplayContactUsPage();
-                break;
-            case "Contact-List":
-                DisplayContactListPage();
-                break;
-            case "Parth":
-                DisplayParthPage();
-                break;
-            case "Yash":
-                DisplayYashPage();
-                break;
-    
-        }
+          case "Contact Us":
+            DisplayContactPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Products":
+            DisplayProductsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
+          case "Edit":
+            DisplayEditPage();
+            break;
+          case "Login":
+            DisplayLoginPage();
+            break;
+          case "Register":
+            DisplayRegisterPage();
+            break;
 
-        let AboutUsButton = document.getElementById("AboutUsButton");
-        console.log(AboutUsButton);
+        }
     }
+
     window.addEventListener("load", Start);
-    
+
 })();
--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,64 +1,70 @@
-class Contact 
-{
-    // public properties (accessors / mutators)
-    get FullName()
-    {
-        return this.m_fullName;
-    }
-    set FullName(fullName)
-    {
-        this.m_fullName = fullName;
-    }
+(function (core) {
 
-    get ContactNumber()
+    class Contact 
     {
-        return this.m_contactNumber;
-    }
-    set ContactNumber(contactNumber)
-    {
-        this.m_contactNumber = contactNumber;
-    }
-
-    get EmailAddress()
-    {
-        return this.m_emailAddress;
-    }
-    set EmailAddress(emailAddress)
-    {
-        this.m_emailAddress = emailAddress;
-    }
-
-    // constructor
-    constructor(fullname = "", contactNumber = "", emailAddress = "")  // default string parameter
-    {
-        this.FullName = fullname;
-        this.ContactNumber = contactNumber;
-        this.EmailAddress = emailAddress;
-    }
-
-    // public methods
-    serialize()
-    {
-        if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "")
+        // public properties (accessors / mutators)
+        get FullName()
         {
-            return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+            return this.m_fullName;
         }
-        console.error("One or more properties of the contact object are missing or empty");
-        return null;
-        
+        set FullName(fullName)
+        {
+            this.m_fullName = fullName;
+        }
+    
+        get ContactNumber()
+        {
+            return this.m_contactNumber;
+        }
+        set ContactNumber(contactNumber)
+        {
+            this.m_contactNumber = contactNumber;
+        }
+    
+        get EmailAddress()
+        {
+            return this.m_emailAddress;
+        }
+        set EmailAddress(emailAddress)
+        {
+            this.m_emailAddress = emailAddress;
+        }
+    
+        // constructor
+        constructor(fullname = "", contactNumber = "", emailAddress = "")  // default string parameter
+        {
+            this.FullName = fullname;
+            this.ContactNumber = contactNumber;
+            this.EmailAddress = emailAddress;
+        }
+    
+        // public methods
+        serialize()
+        {
+            if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "")
+            {
+                return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+            }
+            console.error("One or more properties of the contact object are missing or empty");
+            return null;
+            
+        }
+    
+        deserialize(data) // assume that the data object is comma-seperated list of properties
+        {
+            let propertyArray = data.split(",");
+            this.FullName = propertyArray[0];
+            this.ContactNumber = propertyArray[1];
+            this.EmailAddress = propertyArray[2];
+        }
+    
+        // overridden methods
+        toString()
+        {
+            return `Full Name: ${this.FullName}\n Contact Number: ${this.ContactNumber}\n Email Address: ${this.EmailAddress}`;
+        }
     }
 
-    deserialize(data) // assume that the data object is comma-seperated list of properties
-    {
-        let propertyArray = data.split(",");
-        this.FullName = propertyArray[0];
-        this.ContactNumber = propertyArray[1];
-        this.EmailAddress = propertyArray[2];
-    }
+    core.Contact = Contact;
+}) (core || (core = {}));
 
-    // overridden methods
-    toString()
-    {
-        return `Full Name: ${this.FullName}\n Contact Number: ${this.ContactNumber}\n Email Address: ${this.EmailAddress}`;
-    }
-}
