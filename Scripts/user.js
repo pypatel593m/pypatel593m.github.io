ebc3f0aefd5bf4f4c8ea4231a46061f66ea98d3a
pypatel593m <pypatel593m@gmail.com>
2022-10-07 17:44:54 -0400

    added files from heroku repo

--- Scripts/user.js
+++ Scripts/user.js
@@ -1,73 +1,99 @@
-"use strict";
-var core;
-(function (core) {
-    class User {
-        m_displayName;
-        m_emailAddress;
-        m_username;
-        m_password;
-        get DisplayName() {
-            return this.m_displayName;
+(function(core){
+
+    class User
+    {
+        get FirstName()
+        {
+            return this.m_First_Name;
         }
-        set DisplayName(name) {
-            this.m_displayName = name;
+    
+        set FirstName(first_name)
+        {
+            this.m_First_Name = first_name;
         }
-        get EmailAddress() {
+    
+        get LastName()
+        {
+            return this.m_Last_Name;
+        }
+    
+        set LastName(last_name)
+        {
+            this.m_Last_Name = last_name;
+        }
+    
+        get EmailAddress()
+        {
             return this.m_emailAddress;
         }
-        set EmailAddress(email_address) {
+    
+        set EmailAddress(email_address)
+        {
             this.m_emailAddress = email_address;
         }
-        get Username() {
-            return this.m_username;
+
+        get Password()
+        {
+            return this.m_Password;
         }
-        set Username(username) {
-            this.m_username = username;
+
+        set Password(password)
+        {
+            this.m_Password = password;
         }
-        get Password() {
-            return this.m_password;
+
+        // constructor
+        constructor(displayName = "", emailAddress= "", username = "", password = "")
+        {
+            this.DisplayName = displayName;
+            this.EmailAddress = emailAddress;
+            this.Username = username;
+            this.Password = password;
         }
-        set Password(password) {
-            this.m_password = password;
+
+        // overriden functions
+        toString()
+        {
+            return `Display Name  : ${this.DisplayName}\nEmail Address : ${this.EmailAddress}\nUsername : ${this.Username}`;
         }
-        constructor(displayName = "", emailAddress = "", username = "", password = "") {
-            this.m_displayName = displayName;
-            this.m_emailAddress = emailAddress;
-            this.m_username = username;
-            this.m_password = password;
-        }
-        toString() {
-            return `Display Name    : ${this.DisplayName} \nEmail Address : ${this.EmailAddress} \nUsername : ${this.Username}`;
-        }
-        toJSON() {
+
+        // utility functions
+        toJSON()
+        {
             return {
                 "DisplayName": this.DisplayName,
                 "EmailAddress": this.EmailAddress,
                 "Username": this.Username
-            };
+            }
         }
-        fromJSON(data) {
+
+        fromJSON(data)
+        {
             this.DisplayName = data.DisplayName;
             this.EmailAddress = data.EmailAddress;
             this.Username = data.Username;
             this.Password = data.Password;
         }
-        serialize() {
-            if (this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "") {
+
+        serialize() 
+        {
+            if (this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "") 
+            {
                 return `${this.DisplayName},${this.EmailAddress},${this.Username}`;
             }
-            else {
-                console.error("One or more properties of the User is empty");
-                return null;
-            }
+            console.error("One or more properties of the User Object are missing or empty");
+            return null;
         }
-        deserialize(data) {
+
+        deserialize(data) 
+        {
             let propertyArray = data.split(",");
             this.DisplayName = propertyArray[0];
             this.EmailAddress = propertyArray[1];
             this.Username = propertyArray[2];
         }
     }
+
     core.User = User;
-})(core || (core = {}));
-//# sourceMappingURL=user.js.map
+
+})(core || (core={}));
